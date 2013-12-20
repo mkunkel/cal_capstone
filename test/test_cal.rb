3f3d9e9ed40a7281fb99067f16ec6ecf469c495a
Mike Kunkel <mike@mkunkel.com>
2013-12-20 08:32:07 -0600

    more tests for cal.rb

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -18,7 +18,42 @@ EOS
     assert_equal expected_output, shell_output
   end
 
+  def test_number_of_days_returns_correct_number
+    assert_equal 28, number_of_days("february", 2013)
+    assert_equal 30, number_of_days("april", 2013)
+    assert_equal 30, number_of_days("june", 2013)
+    assert_equal 30, number_of_days("september", 2013)
+    assert_equal 30, number_of_days("november", 2013)
+    assert_equal 31, number_of_days("january", 2013)
+    assert_equal 31, number_of_days("march", 2013)
+    assert_equal 31, number_of_days("may", 2013)
+    assert_equal 31, number_of_days("july", 2013)
+    assert_equal 31, number_of_days("august", 2013)
+    assert_equal 31, number_of_days("october", 2013)
+    assert_equal 31, number_of_days("december", 2013)
+  end
 
+  def test_number_of_days_returns_correct_number_in_leap_year
+    assert_equal 28, number_of_days("february", 2012)
+    assert_equal 30, number_of_days("april", 2012)
+    assert_equal 30, number_of_days("june", 2012)
+    assert_equal 30, number_of_days("september", 2012)
+    assert_equal 30, number_of_days("november", 2012)
+    assert_equal 31, number_of_days("january", 2012)
+    assert_equal 31, number_of_days("march", 2012)
+    assert_equal 31, number_of_days("may", 2012)
+    assert_equal 31, number_of_days("july", 2012)
+    assert_equal 31, number_of_days("august", 2012)
+    assert_equal 31, number_of_days("october", 2012)
+    assert_equal 31, number_of_days("december", 2012)
+  end
+
+  def test_is_leap_year_returns_correctly
+    assert_equal true, is_leap_year?(2012)
+    assert_equal false, is_leap_year?(2013)
+    assert_equal true, is_leap_year?(2000)
+    assert_equal false, is_leap_year?(1900)
+  end
 
 
 end
