cb82d405d18f8d81c337c8db6edc00b9ade5a58a
Mike Kunkel <mike@mkunkel.com>
2013-12-26 13:46:23 -0600

    refactored cal methods into CalHelper class

--- cal.rb
+++ cal.rb
@@ -1,71 +1,4 @@
-require_relative 'zellers'
-require_relative 'date_parser'
-WEEK_LENGTH = 7
-
-
-
-
-
-def get_month_text month, year
-  month = month.capitalize
-  start_day = Zellers.get_day month.to_s + '/1/' + year.to_s
-  start_day = Zellers.days_from_sunday start_day
-  month_days = number_of_days month, year
-  output = "#{month} #{year}".center(20) + "  "
-  output << "\nSu Mo Tu We Th Fr Sa  \n"
-
-  output << get_date_line(1, month_days, start_day)
-  5.times do |index|
-    start = WEEK_LENGTH - (start_day - 1) + (WEEK_LENGTH * index)
-    output << get_date_line(start, month_days, 0)
-  end
-  return output
-end
-
-def get_date_line start_from, max, offset = 0
-  output = ''
-  counter = 0
-  7.times do |index|
-    output << ' ' unless index == 0
-    next_num = start_from + index - offset
-    if index < offset
-      output << '  '
-    elsif next_num > max
-      output << '  '
-    elsif next_num < 10
-      output << ' ' + next_num.to_s
-    else
-      output << next_num.to_s
-    end
-  end
-  return output + "  \n"
-end
-
-def number_of_days month, year
-  month = month.downcase
-
-  if month == 'february'
-    # puts is_leap_year?(year)
-    is_leap_year?(year) ? 29 : 28
-  elsif ['april', 'june', 'september', 'november'].index(month)
-    30
-  elsif ['june', 'september', 'november', 'january', 'march', 'may', 'july', 'august', 'october', 'december'].index(month)
-    31
-  end
-end
-
-def is_leap_year? year
-  year = year.to_i
-  if year % 400 == 0
-    true
-  elsif year % 100 == 0
-    false
-  elsif year % 4 == 0
-    true
-  else
-    false
-  end
-end
+require_relative 'cal_helper'
 
 if ARGV.length == 1
   # Assume user supplied only a year
@@ -83,7 +16,7 @@ elsif ARGV.length == 2
       # User supplied an invalid argument
     end
   end
-  puts get_month_text month, ARGV[1]
+  puts CalHelper.get_month_text month, ARGV[1]
 elsif ARGV.length > 2
   # User supplied too many arguments
 end
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -1,58 +1,59 @@
 require_relative '../cal'
+require_relative '../cal_helper'
 require 'minitest/autorun'
 
 class TestCal < MiniTest::Unit::TestCase
   def test_a_month_in_a_non_leap_year
     expected_output = `cal 2 2013`
-    assert_equal expected_output, get_month_text('february', 2013)
+    assert_equal expected_output, CalHelper.get_month_text('february', 2013)
   end
 
   def test_a_month_in_a_leap_year
     expected_output = `cal 2 2012`
-    assert_equal expected_output, get_month_text("february", 2012)
+    assert_equal expected_output, CalHelper.get_month_text("february", 2012)
   end
 
   def test_number_of_days_returns_correct_number
-    assert_equal 28, number_of_days("february", 2013)
-    assert_equal 30, number_of_days("april", 2013)
-    assert_equal 30, number_of_days("june", 2013)
-    assert_equal 30, number_of_days("september", 2013)
-    assert_equal 30, number_of_days("november", 2013)
-    assert_equal 31, number_of_days("january", 2013)
-    assert_equal 31, number_of_days("march", 2013)
-    assert_equal 31, number_of_days("may", 2013)
-    assert_equal 31, number_of_days("july", 2013)
-    assert_equal 31, number_of_days("august", 2013)
-    assert_equal 31, number_of_days("october", 2013)
-    assert_equal 31, number_of_days("december", 2013)
+    assert_equal 28, CalHelper.number_of_days("february", 2013)
+    assert_equal 30, CalHelper.number_of_days("april", 2013)
+    assert_equal 30, CalHelper.number_of_days("june", 2013)
+    assert_equal 30, CalHelper.number_of_days("september", 2013)
+    assert_equal 30, CalHelper.number_of_days("november", 2013)
+    assert_equal 31, CalHelper.number_of_days("january", 2013)
+    assert_equal 31, CalHelper.number_of_days("march", 2013)
+    assert_equal 31, CalHelper.number_of_days("may", 2013)
+    assert_equal 31, CalHelper.number_of_days("july", 2013)
+    assert_equal 31, CalHelper.number_of_days("august", 2013)
+    assert_equal 31, CalHelper.number_of_days("october", 2013)
+    assert_equal 31, CalHelper.number_of_days("december", 2013)
   end
 
   def test_number_of_days_returns_correct_number_in_leap_year
-    assert_equal 29, number_of_days("february", 2012)
-    assert_equal 30, number_of_days("april", 2012)
-    assert_equal 30, number_of_days("june", 2012)
-    assert_equal 30, number_of_days("september", 2012)
-    assert_equal 30, number_of_days("november", 2012)
-    assert_equal 31, number_of_days("january", 2012)
-    assert_equal 31, number_of_days("march", 2012)
-    assert_equal 31, number_of_days("may", 2012)
-    assert_equal 31, number_of_days("july", 2012)
-    assert_equal 31, number_of_days("august", 2012)
-    assert_equal 31, number_of_days("october", 2012)
-    assert_equal 31, number_of_days("december", 2012)
+    assert_equal 29, CalHelper.number_of_days("february", 2012)
+    assert_equal 30, CalHelper.number_of_days("april", 2012)
+    assert_equal 30, CalHelper.number_of_days("june", 2012)
+    assert_equal 30, CalHelper.number_of_days("september", 2012)
+    assert_equal 30, CalHelper.number_of_days("november", 2012)
+    assert_equal 31, CalHelper.number_of_days("january", 2012)
+    assert_equal 31, CalHelper.number_of_days("march", 2012)
+    assert_equal 31, CalHelper.number_of_days("may", 2012)
+    assert_equal 31, CalHelper.number_of_days("july", 2012)
+    assert_equal 31, CalHelper.number_of_days("august", 2012)
+    assert_equal 31, CalHelper.number_of_days("october", 2012)
+    assert_equal 31, CalHelper.number_of_days("december", 2012)
   end
 
   def test_is_leap_year_returns_correctly
-    assert_equal true, is_leap_year?(2012)
-    assert_equal false, is_leap_year?(2013)
-    assert_equal true, is_leap_year?(2000)
-    assert_equal false, is_leap_year?(1900)
+    assert_equal true, CalHelper.is_leap_year?(2012)
+    assert_equal false, CalHelper.is_leap_year?(2013)
+    assert_equal true, CalHelper.is_leap_year?(2000)
+    assert_equal false, CalHelper.is_leap_year?(1900)
   end
 
   def test_get_date_line
-    assert_equal " 1  2  3  4  5  6  7  \n", get_date_line(1,8)
-    assert_equal " 1  2  3  4  5  6  7  \n", get_date_line(1,7)
-    assert_equal "    1  2  3  4  5  6  \n", get_date_line(1,7,1)
+    assert_equal " 1  2  3  4  5  6  7  \n", CalHelper.get_date_line(1,8)
+    assert_equal " 1  2  3  4  5  6  7  \n", CalHelper.get_date_line(1,7)
+    assert_equal "    1  2  3  4  5  6  \n", CalHelper.get_date_line(1,7,1)
   end
 
 end
