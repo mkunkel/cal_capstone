5dcaf170b5adac19135f4d8a0445bf10dd9f491f
Mike Kunkel <mike@mkunkel.com>
2014-01-09 14:55:50 -0600

    fewer asserts per test

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -4,14 +4,6 @@ require 'minitest/autorun'
 
 class TestCal < MiniTest::Unit::TestCase
 
-  # def get_output file, command = ""
-  #   IO.popen('ruby ' +  file, 'r+') do |pipe|
-  #     pipe.puts(command)
-  #     pipe.close_write
-  #     return pipe.read
-  #   end
-  # end
-
   def test_a_month_in_a_non_leap_year
     expected_output = `cal 2 2013`
     assert_equal expected_output.split("\n"), CalHelper.get_month_text(2, 2013)
@@ -22,79 +14,93 @@ class TestCal < MiniTest::Unit::TestCase
     assert_equal expected_output.split("\n"), CalHelper.get_month_text(2, 2012)
   end
 
-  def test_number_of_days_returns_correct_number
+  def test_number_of_days_returns_correct_number_for_february
     assert_equal 28, CalHelper.number_of_days(2, 2013)
+  end
+
+  def test_number_of_days_returns_correct_number_for_april
     assert_equal 30, CalHelper.number_of_days(4, 2013)
-    assert_equal 30, CalHelper.number_of_days(6, 2013)
-    assert_equal 30, CalHelper.number_of_days(9, 2013)
-    assert_equal 30, CalHelper.number_of_days(11, 2013)
+  end
+
+  def test_number_of_days_returns_correct_number_for_january
     assert_equal 31, CalHelper.number_of_days(1, 2013)
-    assert_equal 31, CalHelper.number_of_days(3, 2013)
-    assert_equal 31, CalHelper.number_of_days(5, 2013)
-    assert_equal 31, CalHelper.number_of_days(7, 2013)
-    assert_equal 31, CalHelper.number_of_days(8, 2013)
-    assert_equal 31, CalHelper.number_of_days(10, 2013)
-    assert_equal 31, CalHelper.number_of_days(12, 2013)
   end
 
-  def test_number_of_days_returns_correct_number_in_leap_year
+  def test_number_of_days_returns_correct_number_in_leap_year_for_february
     assert_equal 29, CalHelper.number_of_days(2, 2012)
-    assert_equal 30, CalHelper.number_of_days(4, 2012)
-    assert_equal 30, CalHelper.number_of_days(6, 2012)
-    assert_equal 30, CalHelper.number_of_days(9, 2012)
-    assert_equal 30, CalHelper.number_of_days(11, 2012)
-    assert_equal 31, CalHelper.number_of_days(1, 2012)
-    assert_equal 31, CalHelper.number_of_days(3, 2012)
-    assert_equal 31, CalHelper.number_of_days(5, 2012)
-    assert_equal 31, CalHelper.number_of_days(7, 2012)
-    assert_equal 31, CalHelper.number_of_days(8, 2012)
-    assert_equal 31, CalHelper.number_of_days(10, 2012)
-    assert_equal 31, CalHelper.number_of_days(12, 2012)
   end
 
-  def test_is_leap_year_returns_correctly
+  def test_number_of_days_returns_correct_number_in_leap_year_for_april
+    assert_equal 30, CalHelper.number_of_days(4, 2012)
+  end
+
+  def test_number_of_days_returns_correct_number_in_leap_year_for_january
+    assert_equal 31, CalHelper.number_of_days(1, 2012)
+  end
+
+
+  def test_is_leap_year_returns_true_for_normal_leap_year
     assert_equal true, CalHelper.is_leap_year?(2012)
+  end
+
+  def test_is_leap_year_returns_false_for_normal_non_leap_year
     assert_equal false, CalHelper.is_leap_year?(2013)
+  end
+
+  def test_is_leap_year_returns_true_for_leap_century
     assert_equal true, CalHelper.is_leap_year?(2000)
+  end
+
+  def test_is_leap_year_returns_false_for_non_leap_century
     assert_equal false, CalHelper.is_leap_year?(1900)
   end
 
   def test_get_date_line
     assert_equal " 1  2  3  4  5  6  7  ", CalHelper.get_date_line(1,8)
-    assert_equal " 1  2  3  4  5  6  7  ", CalHelper.get_date_line(1,7)
+  end
+
+def test_get_date_line_with_offset
     assert_equal "    1  2  3  4  5  6  ", CalHelper.get_date_line(1,7,1)
   end
 
-  def test_implementation_with_month_and_year
+  def test_implementation_with_leap_february
     assert_equal `cal 2 2012`, `ruby cal.rb 2 2012`
+  end
+
+  def test_implementation_with_month_starting_on_saturday
     assert_equal `cal 9 2012`, `ruby cal.rb 9 2012`
+  end
+
+  def test_implementation_with_4_line_month
     assert_equal `cal 2 2015`, `ruby cal.rb 2 2015`
+  end
+
+  def test_implementation_with_non_leap_century_february
     assert_equal `cal 2 1900`, `ruby cal.rb 2 1900`
+  end
+
+  def test_implementation_leap_february
     assert_equal `cal 2 2000`, `ruby cal.rb 2 2000`
-    # output = `ruby cal.rb`
-    # assert_includes `ruby cal.rb`, /ArgumentError/
-    # assert_equal `cal 2 1799`, `ruby cal.rb 2 1799`
-    # assert_equal `cal 2 3001`, `ruby cal.rb 2 3001`
   end
 
   def test_implementation_with_month_word
     assert_equal `cal february 2012`, `ruby cal.rb february 2012`
-    assert_equal `cal September 2012`, `ruby cal.rb September 2012`
-    assert_equal `cal May 2015`, `ruby cal.rb May 2015`
-    assert_equal `cal ApRiL 1900`, `ruby cal.rb ApRiL 1900`
-    assert_equal `cal June 2000`, `ruby cal.rb June 2000`
-    # assert_equal `cal November 1799`, `ruby cal.rb November 1799`
-    # assert_equal `cal JANUARY 3001`, `ruby cal.rb JANUARY 3001`
   end
 
-  def test_implementation_with_only_year
+  def test_implementation_with_only_leap_year
     assert_equal `cal 2012`, `ruby cal.rb 2012`
-    assert_equal `cal 2012`, `ruby cal.rb 2012`
+  end
+
+  def test_implementation_with_only_non_leap_year
     assert_equal `cal 2015`, `ruby cal.rb 2015`
+  end
+
+  def test_implementation_with_only_non_leap_century
     assert_equal `cal 1900`, `ruby cal.rb 1900`
-    assert_equal `cal 2000`, `ruby cal.rb 2000`
-    # assert_equal `cal 1799`, `ruby cal.rb 1799`
-    # assert_equal `cal 3001`, `ruby cal.rb 3001`
+  end
+
+  def test_implementation_with_only_leap_century
+    assert_equal `cal 1900`, `ruby cal.rb 1900`
   end
 
 end
--- test/test_zellers.rb
+++ test/test_zellers.rb
@@ -10,19 +10,9 @@ class TestZellers < MiniTest::Unit::TestCase
     assert_equal 0, Zellers.get_day("12/21/2013")
     assert_equal 1, Zellers.get_day("12/22/2013")
     assert_equal 2, Zellers.get_day("12/23/2013")
-    assert_equal 3, Zellers.get_day("12/24/2013")
-    assert_equal 4, Zellers.get_day("12/25/2013")
-    assert_equal 5, Zellers.get_day("12/26/2013")
-    assert_equal 6, Zellers.get_day("12/27/2013")
-    assert_equal 6, Zellers.get_day("2/1/2013")
   end
 
   def test_zellers_returns_correct_day_in_leap_year
-    assert_equal 0, Zellers.get_day("12/22/2012")
-    assert_equal 1, Zellers.get_day("12/23/2012")
-    assert_equal 2, Zellers.get_day("12/24/2012")
-    assert_equal 3, Zellers.get_day("12/25/2012")
-    assert_equal 4, Zellers.get_day("12/26/2012")
     assert_equal 5, Zellers.get_day("12/27/2012")
     assert_equal 6, Zellers.get_day("12/28/2012")
     assert_equal 4, Zellers.get_day("2/1/2012")
@@ -32,10 +22,6 @@ class TestZellers < MiniTest::Unit::TestCase
     assert_equal 6, Zellers.days_from_sunday(0)
     assert_equal 0, Zellers.days_from_sunday(1)
     assert_equal 1, Zellers.days_from_sunday(2)
-    assert_equal 2, Zellers.days_from_sunday(3)
-    assert_equal 3, Zellers.days_from_sunday(4)
-    assert_equal 4, Zellers.days_from_sunday(5)
-    assert_equal 5, Zellers.days_from_sunday(6)
   end
 
   def test_zellers_has_a_get_day_as_string_method
@@ -46,10 +32,6 @@ class TestZellers < MiniTest::Unit::TestCase
     assert_equal "Saturday", Zellers.get_day_as_string(0)
     assert_equal "Sunday", Zellers.get_day_as_string(1)
     assert_equal "Monday", Zellers.get_day_as_string(2)
-    assert_equal "Tuesday", Zellers.get_day_as_string(3)
-    assert_equal "Wednesday", Zellers.get_day_as_string(4)
-    assert_equal "Thursday", Zellers.get_day_as_string(5)
-    assert_equal "Friday", Zellers.get_day_as_string(6)
   end
 
 
